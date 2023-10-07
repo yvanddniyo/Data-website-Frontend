5ead183620527429d996af79918323f592a1ce7c
nyvan80076@gmail.com <nyvan80076@gmail.com>
2023-10-08 00:24:39 +0300

    correcting some erroes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState} from 'react'
-import { AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Navbar = () => {
   const [nav, setnav] = useState(true)
@@ -18,7 +19,7 @@ const Navbar = () => {
         <li className='p-4 cursor-pointer'>Contact</li>
       </ul>
       <div onClick={handleNav} className='block md:hidden cursor-pointer'>
-        {nav ? <AiOutlineMenu size={20} /> :  <AiOutlineClose size={20}  />}
+        {nav ? <MenuIcon size={20} /> :  <CloseIcon size={20}  />}
       </div>
       <div className={!nav ? `fixed left-0 top-0 w-[60%] h-full border-r-gray-900 bg-[#000300] ease-in-out duration-500 md:left-[-100%] ` : " fixed left-[-100%]"}>
       <h1 className=" w-full text-3xl font-bold text-[#00df9a] m-4 ">React.</h1>
